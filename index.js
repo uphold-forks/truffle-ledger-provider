765b89bf4481f131912d0878f8a076d570c7bced
Pedro Branco <pedro.branco@uphold.com>
2019-01-22 11:05:46 +0000

    Add support for truffle@5

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('babel-polyfill')
+
 /**
  * Module dependencies.
  */
@@ -8,7 +10,7 @@ const FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js
 const ProviderEngine = require('web3-provider-engine')
 const ProviderSubprovider = require('web3-provider-engine/subproviders/provider.js')
 const TransportU2F = require('@ledgerhq/hw-transport-node-hid').default
-const Web3 = require('web3')
+const Web3HttpProvider = require('web3-providers-http')
 const createLedgerSubprovider = require('@ledgerhq/web3-subprovider').default
 
 /**
@@ -27,7 +29,7 @@ module.exports = class LedgerProvider extends ProviderEngine {
       return transport
     }, options))
     this.addProvider(new FiltersSubprovider())
-    this.addProvider(new ProviderSubprovider(new Web3.providers.HttpProvider(url)))
+    this.addProvider(new ProviderSubprovider(new Web3HttpProvider(url)))
     this.start()
   }
 }
